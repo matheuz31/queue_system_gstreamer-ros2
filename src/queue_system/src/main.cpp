44df9116cb8f7000e271f680e5e4967528d9f739
matheuz31 <matheusdescorrea@gmail.com>
2024-05-22 21:43:58 -0300

    Adding a simple ros image publisher

--- src/queue_system/src/main.cpp
+++ src/queue_system/src/main.cpp
@@ -1,5 +1,30 @@
 #include <gst/gst.h>
 #include <iostream>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+
+class SimpleImagePublisher : public rclcpp::Node {
+public:
+    SimpleImagePublisher()
+    : Node("simple_image_publisher") {
+        publisher_ = this->create_publisher<sensor_msgs::msg::Image>("camera/image", 10);
+    }
+
+    void publish_image(const uint8_t* data, size_t width, size_t height) {
+        auto message = sensor_msgs::msg::Image();
+        message.header.stamp = this->now();
+        message.header.frame_id = "camera_frame";
+        message.height = height;
+        message.width = width;
+        message.encoding = "rgb8";
+        message.step = width * 3; 
+        message.data = std::vector<uint8_t>(data, data + (width * height * 3));
+        publisher_->publish(message);
+    }
+
+private:
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+};
 
 static void on_pad_added(GstElement *element, GstPad *pad, gpointer data) {
     GstPad *sinkpad;
@@ -12,16 +37,26 @@ static void on_pad_added(GstElement *element, GstPad *pad, gpointer data) {
     gst_object_unref(sinkpad);
 }
 
+static GstPadProbeReturn probe_callback(GstPad *pad, GstPadProbeInfo *info, gpointer user_data){
+
+    return GST_PAD_PROBE_PASS;
+}
+
 int main(int argc, char *argv[]) {
     GstElement *pipeline, *source, *video_sink, *decoder, *converter;
     GMainLoop *loop;
     GstBus *bus;
     GstMessage *msg;
+    GstPad *srcpad;
     GError *error = NULL;
 
     gst_init(&argc, &argv);
 
-    if(argc =! 2){
+    rclcpp::init(argc, argv);
+
+    auto node = std::make_shared<SimpleImagePublisher>();
+
+    if(argc != 3){
         std::cerr << "you need <Video File Path> as" << argv[0] << std::endl;
 
     }
@@ -33,17 +68,27 @@ int main(int argc, char *argv[]) {
 
     pipeline = gst_pipeline_new("pipeline_ros2");
 
-    if(!pipeline || !source || !decoder || !video_sink || converter){
+    if(!pipeline || !source || !decoder || !video_sink || !converter){
         g_printerr("Not all elements could be created");
     }
-
     g_object_set(G_OBJECT(source), "location", argv[1], NULL);
 
-    gst_bin_add_many(GST_BIN(pipeline), source, decoder, converter, video_sink);
-
+    gst_bin_add(GST_BIN(pipeline), source);
+    gst_bin_add(GST_BIN(pipeline), decoder);
+    gst_bin_add(GST_BIN(pipeline), converter);
+    
+    gst_bin_add(GST_BIN(pipeline), video_sink);
     gst_element_link(source, decoder);
+
     g_signal_connect(decoder, "pad-added", G_CALLBACK(on_pad_added), converter);
+
+    std::cout << "teste" << std::endl;
     gst_element_link(converter, video_sink);
+
+    srcpad = gst_element_get_static_pad(converter, "src");
+    gst_pad_add_probe(srcpad, GST_PAD_PROBE_TYPE_BUFFER, probe_callback, NULL, NULL);
+    gst_object_unref(srcpad);
+
 
     loop = g_main_loop_new(NULL, FALSE);
 
@@ -54,6 +99,10 @@ int main(int argc, char *argv[]) {
     gst_element_set_state(pipeline, GST_STATE_NULL);
     gst_object_unref(GST_OBJECT(pipeline));
     g_main_loop_unref(loop);
+
+    rclcpp::spin(node);
+
+    rclcpp::shutdown();
 
     return 0;
 }
